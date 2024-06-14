fb42f4ba56f04e6f1723fc0d29830cbeec98a3e7
Wonhui Roh <rwhui012@gmail.com>
2024-06-14 18:10:42 +0900

    [FIX] CommonDialog => single/multi-lined CommonDialog 의 구분

--- app/src/main/java/com/youngnrich/android/CommonDialog.kt
+++ app/src/main/java/com/youngnrich/android/CommonDialog.kt
@@ -1,6 +1,7 @@
 package com.youngnrich.android
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +10,13 @@ import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import com.youngnrich.android.databinding.DialogCommonBinding
 
+private const val TAG = "CommonDialog"
+
 class CommonDialog: DialogFragment {
     private var dialogText: Int? = null
     private var dialogTexts: List<Int>? = null
+
+    private var dialogTextsIndex = 0
 
     constructor(dialogText: Int) {
         this.dialogText = dialogText
@@ -39,16 +44,40 @@ class CommonDialog: DialogFragment {
         layoutParams.y = 28 // 하단 여백
         window?.attributes = layoutParams
 
-        binding.closeDialogButton.setOnClickListener {
-            dismiss()
-        }
+        binding.apply {
+            closeDialogButton.setOnClickListener {
+                dismiss()
+            }
 
-        if (dialogText != null) {
-            binding.nextDialogImageButton.visibility = View.INVISIBLE
-            binding.commonDialogTextView.setText(dialogText!!)
-        }
-        if (dialogTexts != null) {
-            // TODO: next 버튼 활성화 & next 버튼 눌렀을 때 다음 텍스트 나타나게 & 마지막 텍스트에서는 다시 next 버튼 비활성화
+            if (dialogText != null) {
+                Log.d(TAG, "Single-line Dialog is gonna be opened")
+
+                nextDialogImageButton.visibility = View.INVISIBLE
+                closeDialogFrameLayout.visibility = View.VISIBLE
+                commonDialogTextView.setText(dialogText!!)
+            }
+
+            if (dialogTexts != null) {
+                Log.d(TAG, "Multi-line Dialog is gonna be opened")
+
+                // next 버튼 활성화 & close 버튼 비활성화
+                nextDialogImageButton.visibility = View.VISIBLE
+                closeDialogFrameLayout.visibility = View.INVISIBLE
+                commonDialogTextView.setText(dialogTexts!![dialogTextsIndex])
+
+                nextDialogImageButton.setOnClickListener {
+                    // next 버튼 눌렀을 때 다이얼로그의 다음 텍스트 나타나게 & 다이얼로그의 마지막에서는 next 버튼 비활성화 & close 버튼 활성화
+
+                    dialogTextsIndex++
+
+                    if (dialogTextsIndex == dialogTexts?.lastIndex) {
+                        nextDialogImageButton.visibility = View.INVISIBLE
+                        closeDialogFrameLayout.visibility = View.VISIBLE
+                    }
+
+                    commonDialogTextView.setText(dialogTexts!![dialogTextsIndex])
+                }
+            }
         }
 
         return binding.root
