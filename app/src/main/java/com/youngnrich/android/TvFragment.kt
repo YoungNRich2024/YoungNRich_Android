bf0f8efc5370275d0d9101f1878b66fdeb67dd88
Wonhui Roh <rwhui012@gmail.com>
2024-06-14 18:34:56 +0900

    [REFACTOR/FIX/BUG] TvFragment (Clean ver.)
    - WebView 를 사용할 시, 동영상을 재생하고 다시 hosting activity 로 되돌아가면 inventory 에서 리모콘만 클릭이 안 되는 기이한 현상이 있었음
    - WebView 를 주석 처리하면 정상적으로 동작하는 것으로 보아 WebView 로 인해 발생하는 문제로 생각됨
    - 일단 서드파티 라이브러리로 해당 문제를 잠재워둠

--- app/src/main/java/com/youngnrich/android/TvFragment.kt
+++ app/src/main/java/com/youngnrich/android/TvFragment.kt
@@ -12,7 +12,7 @@ import com.youngnrich.android.databinding.FragmentTvBinding
 private const val TAG = "TvFragment"
 
 class TvFragment : Fragment() {
-    // TODO 유튜브 영상 로드
+
     companion object {
         fun newInstance() = TvFragment()
     }
@@ -23,61 +23,14 @@ class TvFragment : Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    private val youtubeVideo = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/sv91cHVPBVs?si=W5rCncfL7ihohM5v\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTvBinding.inflate(inflater, container, false)
-//        _baseBinding = ActivityBaseNavigationBinding.bind(binding.root)
 
-//        binding.youtubeWebView.apply {
-//            settings.javaScriptEnabled = true
-//            webChromeClient = WebChromeClient()
-//            loadData(youtubeVideo, "text/html", "utf-8")
-//        }
-//
-//        return binding.root
-
-//        val view = inflater.inflate(R.layout.fragment_tv, container, false)
-
-        // Youtube -- WebView ver.
-//        binding.youtubeWebView.apply {
-//            setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-//
-//            webViewClient = object : WebViewClient() {
-//                override fun onReceivedError(
-//                    view: WebView?,
-//                    request: WebResourceRequest?,
-//                    error: WebResourceError?
-//                ) {
-//                    super.onReceivedError(view, request, error)
-//                    // Handle the error
-//                    Log.e(TAG, "webViewClient Error")
-//                }
-//            }
-//            settings.apply {
-//                javaScriptEnabled = true
-//                domStorageEnabled = true
-//                cacheMode = WebSettings.LOAD_DEFAULT
-//                mediaPlaybackRequiresUserGesture = false
-//                allowFileAccess = true
-//                databaseEnabled = true
-//                //loadWithOverviewMode = true
-//                //useWideViewPort = true
-//            }
-//            //loadData(youtubeVideo, "text/html", "utf-8")
-//            loadUrl(youtubeVideo)
-//        }
-
-        //todo
+        // 유튜브 영상 로드
         viewLifecycleOwner.lifecycle.addObserver(binding.youtubePlayerView)
 
         return binding.root
@@ -92,7 +45,6 @@ class TvFragment : Fragment() {
                 baseArrowRight.visibility = View.GONE
 
                 baseArrowBottom.setOnClickListener {
-                    // todo: 동영상 재생 하고 hosting activity 로 나가면 inventory 가 안 됨
                     binding.root.removeView(youtubePlayerView)
 
                     requireActivity().supportFragmentManager.popBackStack()
@@ -100,30 +52,12 @@ class TvFragment : Fragment() {
                     Log.d(TAG, "go back to hosting Activity, 'FirstRoomGameActivity'")
                 }
             }
-
-
         }
 
     }
 
-    // Youtube -- WebView ver.
-//    override fun onPause() {
-//        super.onPause()
-//        binding.youtubeWebView.onPause()
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        binding.youtubeWebView.onResume()
-//    }
-
     override fun onDestroyView() {
         super.onDestroyView()
-//        binding.youtubeWebView.apply {
-//            clearCache(true)
-//            clearHistory()
-//            destroy()
-//        }
 
         _binding = null
     }
